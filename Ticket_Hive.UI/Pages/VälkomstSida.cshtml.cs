13319a8284f8453ea75957e979e32b205ffdfa6a
fahimrez <113334759+fahimrez@users.noreply.github.com>
2023-04-03 12:58:15 +0200

    hemsida

---
+++ Ticket_Hive.UI/Pages/VälkomstSida.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ticket_Hive.UI.Pages
+{
+    public class VälkomstSidaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
