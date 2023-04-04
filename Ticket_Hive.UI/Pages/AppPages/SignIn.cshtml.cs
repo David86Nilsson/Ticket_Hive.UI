c5d757020ffc67a7ea743a30b55211753cc9196e
Nyagrenen <113024289+Nyagrenen@users.noreply.github.com>
2023-04-04 18:58:43 +0200

    Första sidan klar med knappar som tar en till register och sign in. Även gjort en ny mapp (AppPages) och lagt register och sign in där

---
+++ Ticket_Hive.UI/Pages/AppPages/SignIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ticket_Hive.UI.Pages.AppPages
+{
+    public class SignInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
