f652227e8db6aab628af343f1fa1f0abc1f4673b
David86Nilsson <113332119+David86Nilsson@users.noreply.github.com>
2023-04-05 09:55:41 +0200

    Added packages

--- Ticket_Hive.Data/Migrations/20230403110223_init.cs
+++ Ticket_Hive.Data/Migrations/20230403110223_init.cs
@@ -1,128 +1,127 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Ticket_Hive.Data.Migrations
 {
-    /// <inheritdoc />
-    public partial class init : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Events",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    EventType = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    dateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    Capacity = table.Column<int>(type: "int", nullable: false),
-                    TicketsSold = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Events", x => x.Id);
-                });
+	/// <inheritdoc />
+	public partial class init : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.CreateTable(
+				name: "Events",
+				columns: table => new
+				{
+					Id = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					EventType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					dateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+					Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+					Capacity = table.Column<int>(type: "int", nullable: false),
+					TicketsSold = table.Column<int>(type: "int", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Events", x => x.Id);
+				});
 
-            migrationBuilder.CreateTable(
-                name: "AppUsers",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    EventModelId = table.Column<int>(type: "int", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AppUsers", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_AppUsers_Events_EventModelId",
-                        column: x => x.EventModelId,
-                        principalTable: "Events",
-                        principalColumn: "Id");
-                });
+			migrationBuilder.CreateTable(
+				name: "AppUsers",
+				columns: table => new
+				{
+					Id = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+					EventModelId = table.Column<int>(type: "int", nullable: true)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_AppUsers", x => x.Id);
+					table.ForeignKey(
+						name: "FK_AppUsers_Events_EventModelId",
+						column: x => x.EventModelId,
+						principalTable: "Events",
+						principalColumn: "Id");
+				});
 
-            migrationBuilder.CreateTable(
-                name: "Bookings",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    EventId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Bookings", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Bookings_Events_EventId",
-                        column: x => x.EventId,
-                        principalTable: "Events",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+			migrationBuilder.CreateTable(
+				name: "Bookings",
+				columns: table => new
+				{
+					Id = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+					EventId = table.Column<int>(type: "int", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Bookings", x => x.Id);
+					table.ForeignKey(
+						name: "FK_Bookings_Events_EventId",
+						column: x => x.EventId,
+						principalTable: "Events",
+						principalColumn: "Id",
+						onDelete: ReferentialAction.Cascade);
+				});
 
-            migrationBuilder.CreateTable(
-                name: "AppUserModelBookingModel",
-                columns: table => new
-                {
-                    BookingsId = table.Column<int>(type: "int", nullable: false),
-                    UsersId = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AppUserModelBookingModel", x => new { x.BookingsId, x.UsersId });
-                    table.ForeignKey(
-                        name: "FK_AppUserModelBookingModel_AppUsers_UsersId",
-                        column: x => x.UsersId,
-                        principalTable: "AppUsers",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_AppUserModelBookingModel_Bookings_BookingsId",
-                        column: x => x.BookingsId,
-                        principalTable: "Bookings",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+			migrationBuilder.CreateTable(
+				name: "AppUserModelBookingModel",
+				columns: table => new
+				{
+					BookingsId = table.Column<int>(type: "int", nullable: false),
+					UsersId = table.Column<int>(type: "int", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_AppUserModelBookingModel", x => new { x.BookingsId, x.UsersId });
+					table.ForeignKey(
+						name: "FK_AppUserModelBookingModel_AppUsers_UsersId",
+						column: x => x.UsersId,
+						principalTable: "AppUsers",
+						principalColumn: "Id",
+						onDelete: ReferentialAction.Cascade);
+					table.ForeignKey(
+						name: "FK_AppUserModelBookingModel_Bookings_BookingsId",
+						column: x => x.BookingsId,
+						principalTable: "Bookings",
+						principalColumn: "Id",
+						onDelete: ReferentialAction.Cascade);
+				});
 
-            migrationBuilder.CreateIndex(
-                name: "IX_AppUserModelBookingModel_UsersId",
-                table: "AppUserModelBookingModel",
-                column: "UsersId");
+			migrationBuilder.CreateIndex(
+				name: "IX_AppUserModelBookingModel_UsersId",
+				table: "AppUserModelBookingModel",
+				column: "UsersId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_AppUsers_EventModelId",
-                table: "AppUsers",
-                column: "EventModelId");
+			migrationBuilder.CreateIndex(
+				name: "IX_AppUsers_EventModelId",
+				table: "AppUsers",
+				column: "EventModelId");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Bookings_EventId",
-                table: "Bookings",
-                column: "EventId");
-        }
+			migrationBuilder.CreateIndex(
+				name: "IX_Bookings_EventId",
+				table: "Bookings",
+				column: "EventId");
+		}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "AppUserModelBookingModel");
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+				name: "AppUserModelBookingModel");
 
-            migrationBuilder.DropTable(
-                name: "AppUsers");
+			migrationBuilder.DropTable(
+				name: "AppUsers");
 
-            migrationBuilder.DropTable(
-                name: "Bookings");
+			migrationBuilder.DropTable(
+				name: "Bookings");
 
-            migrationBuilder.DropTable(
-                name: "Events");
-        }
-    }
+			migrationBuilder.DropTable(
+				name: "Events");
+		}
+	}
 }
