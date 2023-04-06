a0ea1a5dec1ad4d56a16a0aadf69904ce48c0061
fahimrez <113334759+fahimrez@users.noreply.github.com>
2023-04-06 14:08:21 +0200

    Adjusted UI and added a logo.

--- Ticket_Hive.UI/Pages/Member/ConfirmationPage.cshtml.cs
+++ Ticket_Hive.UI/Pages/Member/ConfirmationPage.cshtml.cs
@@ -1,11 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ticket_Hive.Data.Models;
 
 namespace Ticket_Hive.UI.Pages.Member
 {
     public class ConfirmationPageModel : PageModel
     {
-        public void OnGet()
+
+
+        public List<EventModel> Events { get; set; } = new()
+        {
+           new EventModel()
+           {
+            
+               Location = "Malmö",
+               Price = 500,
+               Capacity = 10000,
+               
+           },
+
+           new EventModel()
+           {
+			   Location = "Göteborg",
+			   Price = 1100,
+			   Capacity = 30000,
+
+		   }
+		};
+
+		public void OnGet()
         {
         }
     }
