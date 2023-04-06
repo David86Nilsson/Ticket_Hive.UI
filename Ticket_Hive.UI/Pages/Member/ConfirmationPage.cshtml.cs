db1014c1c1bcbe0c83cdc84885bedba8aef2ef2e
fahimrez <113334759+fahimrez@users.noreply.github.com>
2023-04-06 12:33:54 +0200

    Added UI to confirmationpage and commented out part of Program.cs

---
+++ Ticket_Hive.UI/Pages/Member/ConfirmationPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ticket_Hive.UI.Pages.Member
+{
+    public class ConfirmationPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
