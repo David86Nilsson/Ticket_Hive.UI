f45a533de2a8a8ea6b64a9560ed9b6d8942c1b7d
David86Nilsson <113332119+David86Nilsson@users.noreply.github.com>
2023-04-11 14:47:28 +0200

    changes

--- Ticket_Hive.Data/Models/ShoppingCartModel.cs
+++ Ticket_Hive.Data/Models/ShoppingCartModel.cs
@@ -3,7 +3,7 @@
     public class ShoppingCartModel
     {
         public int Id { get; set; }
-        public AppUserModel User { get; set; } = null!;
+        public string User { get; set; } = null!;
         public List<BookingModel> Bookings { get; set; } = new();
         public DateTime Created { get; set; }
     }
