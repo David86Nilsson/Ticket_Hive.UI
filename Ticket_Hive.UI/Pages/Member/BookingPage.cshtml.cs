79f5ace71c1996842f0108004254f0f6c39be72a
Petrus Lundin <petruslundin@live.com>
2023-04-11 15:25:26 +0200

    sbfsdnn

--- Ticket_Hive.UI/Pages/Member/BookingPage.cshtml.cs
+++ Ticket_Hive.UI/Pages/Member/BookingPage.cshtml.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Mono.TextTemplating;
 using Ticket_Hive.Data.Models;
 using Ticket_Hive.Data.Repos;
@@ -16,7 +18,7 @@ namespace Ticket_Hive.UI.Pages.Member
 		private readonly IAppUserModelRepo appUserModelRepo;
 
 		public string search;
-		
+
 
 		public List<EventModel> SearchResults { get; set; }
 
@@ -29,51 +31,38 @@ namespace Ticket_Hive.UI.Pages.Member
 			this.appUserModelRepo = appUserModelRepo;
 		}
 
-		public async Task OnGet()
+		public async Task OnGetAsync(string sortOrder)
 		{
-			// Get all events initially
+
 			Events = await eventRepo.GetAllEventsAsync();
 			SearchResults = Events;
 
-			// Check if a search query is present
+
 			var searchQuery = Request.Query["search"].ToString();
 			if (!string.IsNullOrEmpty(searchQuery))
 			{
-				// Parse the search query into separate terms
+
 				var searchTerms = searchQuery.Split(' ');
 
-				// Set the search properties to the parsed terms
+
 				search = searchTerms.FirstOrDefault();
 
-				// Filter the search results based on the search properties
+
 				SearchResults = SearchResults
 					.Where(e =>
-						(string.IsNullOrEmpty(search) || 
-						e.Name.Contains(search, StringComparison.OrdinalIgnoreCase)  || 
-						e.EventType.Contains(search, StringComparison.OrdinalIgnoreCase)  || 
+						(string.IsNullOrEmpty(search) ||
+						e.Name.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+						e.EventType.Contains(search, StringComparison.OrdinalIgnoreCase) ||
 						e.Location.Contains(search, StringComparison.OrdinalIgnoreCase)))
 					.ToList();
+
 			}
 		}
-
-		//public IActionResult OnPostSearch()
-		//{
-		//	// Call the method to perform the search
-		//	PerformSearch();
-
-		//	return Page();
-		//}
-
-		//private void PerformSearch()
-		//{
-		//	SearchResults = Events
-		//.Where(e =>
-		//				(string.IsNullOrEmpty(search) || 
-		//				e.Name.Contains(search, StringComparison.OrdinalIgnoreCase) || 
-		//				e.EventType.Contains(search, StringComparison.OrdinalIgnoreCase) || 
-		//				e.Location.Contains(search, StringComparison.OrdinalIgnoreCase)))
-		//			.ToList();
-		//}
 	}
 }
+
+				
+
+
+
 
