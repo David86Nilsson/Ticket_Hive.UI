d83e9fc1fbda78bdfd1a7c6d814fc5aaf4257b14
ShappiMoradi <shappi.moradi@hotmail.com>
2023-04-12 19:07:23 +0200

    Admin Page

---
+++ Ticket_Hive.UI/Pages/AppPages/CreateEvent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ticket_Hive.UI.Pages.AppPages
+{
+    public class CreateEventModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
