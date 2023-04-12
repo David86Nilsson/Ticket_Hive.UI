ecf106f4f10a8629b25ef5c193affa420d2cc126
David86Nilsson <113332119+David86Nilsson@users.noreply.github.com>
2023-04-12 14:41:10 +0200

    SignInCodeBehind

--- Ticket_Hive.UI/Pages/AppPages/SignIn.cshtml.cs
+++ Ticket_Hive.UI/Pages/AppPages/SignIn.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
@@ -6,20 +7,37 @@ namespace Ticket_Hive.UI.Pages.AppPages
 {
     public class SignInModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> signInManager;
 
         [BindProperty]
-        
+
         [Required(ErrorMessage = "Invalid Username, please try again")]
         public string UserName { get; set; }
 
 
-        [BindProperty]        
+        [BindProperty]
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Wrong Password, please try again")]
         public string Password { get; set; }
 
+        public SignInModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await signInManager.PasswordSignInAsync("user", "Password1234!", false, false);
+                if (signInResult.Succeeded)
+                {
+                    return RedirectToPage("/Member/Home");
+                }
+            }
+            return Page();
         }
     }
 }
