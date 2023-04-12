090756f98268750f6918352abc37bb7a51c831da
David86Nilsson <113332119+David86Nilsson@users.noreply.github.com>
2023-04-12 08:42:03 +0200

    added html

--- Ticket_Hive.UI/Pages/Member/AdminPage.cshtml.cs
+++ Ticket_Hive.UI/Pages/Member/AdminPage.cshtml.cs
@@ -3,8 +3,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ticket_Hive.UI.Pages.Member
 {
+    [BindProperties]
     public class AdminPageModel : PageModel
     {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Location { get; set; }
+        public string EventType { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Price { get; set; }
+
+        public AdminPageModel()
+        {
+        }
         public void OnGet()
         {
         }
