eb164979c99eb83bd252c49171ead85d667142c1
David86Nilsson <113332119+David86Nilsson@users.noreply.github.com>
2023-04-12 09:42:49 +0200

    added code

--- Ticket_Hive.UI/Pages/Member/AdminPage.cshtml.cs
+++ Ticket_Hive.UI/Pages/Member/AdminPage.cshtml.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ticket_Hive.Data.Models;
+using Ticket_Hive.Data.Repos;
 
 namespace Ticket_Hive.UI.Pages.Member
 {
     [BindProperties]
     public class AdminPageModel : PageModel
     {
+        private readonly IEventModelRepo eventModelRepo;
+
         public string Name { get; set; }
         public int Capacity { get; set; }
         public string Location { get; set; }
@@ -13,11 +17,39 @@ namespace Ticket_Hive.UI.Pages.Member
         public DateTime DateTime { get; set; }
         public decimal Price { get; set; }
 
-        public AdminPageModel()
+        public List<EventModel> Events;
+
+        public int EventToDelete { get; set; }
+
+        public AdminPageModel(IEventModelRepo eventModelRepo)
         {
+            this.eventModelRepo = eventModelRepo;
         }
-        public void OnGet()
+        public async Task OnGet()
         {
+            Events = await eventModelRepo.GetAllEventsAsync();
+        }
+        public async Task<IActionResult> OnPostAdd()
+        {
+            var newEvent = new EventModel
+            {
+                Name = Name,
+                Capacity = Capacity,
+                Location = Location,
+                EventType = EventType,
+                DateTime = DateTime,
+                Price = Price,
+                Image = $"/Images/EventImages/image {new Random().Next(1, 10)}.png"
+            };
+            await eventModelRepo.AddEventAsync(newEvent);
+            Events = await eventModelRepo.GetAllEventsAsync();
+            return Page();
+        }
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await eventModelRepo.DeleteEventAsync(await eventModelRepo.GetEventByIdAsync(EventToDelete));
+            Events = await eventModelRepo.GetAllEventsAsync();
+            return Page();
         }
     }
 }
