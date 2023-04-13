f824df9e3b9fee8925e7ce5daa24f544d902109f
Petrus Lundin <petruslundin@live.com>
2023-04-13 10:36:02 +0200

    Enkel bookingpage

--- Ticket_Hive.UI/Pages/Member/BookingPage.cshtml.cs
+++ Ticket_Hive.UI/Pages/Member/BookingPage.cshtml.cs
@@ -16,43 +16,43 @@ namespace Ticket_Hive.UI.Pages.Member
 		
 		private readonly IEventModelRepo eventRepo;
 
-
+		
 		public List<EventModel> SearchResults { get; set; }
-
 		public List<EventModel> Events { get; set; } = new();
+        public string search { get; set; }
 
-		public BookingPageModel(IEventModelRepo eventRepo)
+        public BookingPageModel(IEventModelRepo eventRepo)
 		{
 			
 			this.eventRepo = eventRepo;
 		}
 
-		public async Task OnGetAsync(string sortOrder)
+		public async Task OnGetAsync()
 		{
 
-			//Events = await eventRepo.GetAllEventsAsync();
-			//SearchResults = Events;
+			Events = await eventRepo.GetAllEventsAsync();
+			SearchResults = Events;
 
 
-			//var searchQuery = Request.Query["search"].ToString();
-			//if (!string.IsNullOrEmpty(searchQuery))
-			//{
+			var searchQuery = Request.Query["search"].ToString();
+			if (!string.IsNullOrEmpty(searchQuery))
+			{
 
-			//	var searchTerms = searchQuery.Split(' ');
-
-
-			//	search = searchTerms.FirstOrDefault();
+				var searchTerms = searchQuery.Split(' ');
 
 
-			//	SearchResults = SearchResults
-			//		.Where(e =>
-			//			(string.IsNullOrEmpty(search) ||
-			//			e.Name.Contains(search, StringComparison.OrdinalIgnoreCase) ||
-			//			e.EventType.Contains(search, StringComparison.OrdinalIgnoreCase) ||
-			//			e.Location.Contains(search, StringComparison.OrdinalIgnoreCase)))
-			//		.ToList();
+				search = searchTerms.FirstOrDefault();
 
-			//}
+
+				SearchResults = SearchResults
+					.Where(e =>
+						(string.IsNullOrEmpty(search) ||
+						e.Name.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+						e.EventType.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+						e.Location.Contains(search, StringComparison.OrdinalIgnoreCase)))
+					.ToList();
+
+			}
 		}
 	}
 }
