1c3dd7c16db316ea0ce7c137e58db9055b788e69
Nyagrenen <113024289+Nyagrenen@users.noreply.github.com>
2023-04-14 00:23:11 +0200

    comments

--- Ticket_Hive.UI/Pages/Index.cshtml.cs
+++ Ticket_Hive.UI/Pages/Index.cshtml.cs
@@ -3,14 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ticket_Hive.UI.Pages
 {
-
+    /// <summary>
+    /// Denna klass är  ansvarig för att hantera användarinteraktioner på startsidan.
+    /// </summary>
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// Hanterar click event för inloggning.
+        /// Omdirigerar användaren till inloggningssidan när knappen "Sign In" klickas.
+        /// </summary>
         protected void btnSignIn_Click(object sender, EventArgs e)
         {
             Response.Redirect("/SignIn");
         }
 
+        /// <summary>
+        /// Hanterar click event för registrering.
+        /// Omdirigerar användaren till registreringssidan när knappen "Register" klickas.
+        /// </summary>
         protected void btnRegister_Click(object sender, EventArgs e)
         {
             Response.Redirect("/Register");
